0aba060c216513f03cb767cced02d94fe73b4f6b
Christy <223317067+chrso25@users.noreply.github.com>
2026-07-16 11:48:48 +0100

    SDEC-459 update shell script

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,4 @@
 
 ENVIRONMENT=$1
 
-sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.api.specs.*"
+sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Denvironment="${ENVIRONMENT:=local}"
