ee7357eed3e25d47af3ae33a372f81a3e38662cf
amityadav-crypto <320564726+amityadav-crypto@users.noreply.github.com>
2026-09-22 13:39:59 +0100

    SDEC-1044 Updated run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -6,5 +6,3 @@ HEADLESSFLAG=$3
 PREVIOUSVERSIONFLAG=$4
 
 sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.option.headless="${HEADLESSFLAG:=false}" -Dbrowser.usePreviousVersion="${PREVIOUSVERSIONFLAG:=true}" "testOnly uk.gov.hmrc.api.specs.*" testReport
-
-sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.api.specs.*"
